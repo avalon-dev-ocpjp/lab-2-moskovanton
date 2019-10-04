245fd038d5be7336ca51edb518f1ee0da61c6f8e
moskovanton <48486058+moskovanton@users.noreply.github.com>
2019-10-04 16:34:44 +0300

    Update FileMoveAction.java

--- src/ru/avalon/java/ocpjp/labs/actions/FileMoveAction.java
+++ src/ru/avalon/java/ocpjp/labs/actions/FileMoveAction.java
@@ -1,12 +1,10 @@
-package ru.avalon.java.ocpjp.labs.actions;
+package ru.avalon.java.actions;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Действие, которое перемещает файлы в пределах дискового
@@ -16,46 +14,36 @@ public class FileMoveAction implements Action {
     /**
      * {@inheritDoc}
      */
-    String filename = "D:\\Users\\Bozhenkov\\lab2\\Exam808_SampleQuestion.pdf";
-    String dest = "D:\\Users\\Bozhenkov\\Exam808_SampleQuestion.pdf";
-    File source = new File(filename);
-    Path p1 = source.toPath();
+    
+    private final Path moveFrom;
+    private final Path moveTo;
 
-    File destination = new File(dest);
-    Path p2 = destination.toPath();
-
-    private void movefile() throws IOException {
-        Files.copy(p1, p2, StandardCopyOption.REPLACE_EXISTING);
+    public FileMoveAction(String moveFrom, String moveTo) {
+        this.moveFrom = Paths.get(moveFrom);
+        this.moveTo = Paths.get(moveTo);
     }
+    
+    private synchronized void move() throws IOException {
+
+        if (!Files.exists(moveFrom)) {
+            System.out.println("Файл не найден.");
+        } else {
+            Files.move(moveFrom, moveTo.resolve(moveFrom.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Перемещение завершено.");
+        }
+    }
+    
     @Override
     public void run() {
-        if (!new File(dest).exists()) {
-            try {
-                Files.copy(p1, p2, StandardCopyOption.REPLACE_EXISTING);
-                System.out.println("New copy was created");
-                if (!new File(dest).exists()){
-                    System.out.println("Original file deletd");
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(FileMoveAction.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } else {
-            try {
-                System.out.println("File is already existed");
-                Files.delete(p2);
-                System.out.println("Previos copy deleted");
-                Files.copy(p1, p2, StandardCopyOption.COPY_ATTRIBUTES);
-                Files.delete(p1);
-                System.out.println("New copy was created");
-                if (!new File(filename).exists()){
-                    System.out.println("Original file deletd");
-                } else {
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(FileMoveAction.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        /*
+         * TODO №4 Реализуйте метод run класса FileMoveAction
+         */
+        try{
+            move();
+        } catch (IOException e) {
+            System.out.println("Перемещение не удалось: " + e.getMessage() + ".");   
         }
-       
+        System.out.print("> ");
     }
 
     /**
@@ -66,7 +54,7 @@ public class FileMoveAction implements Action {
         /*
          * TODO №5 Реализуйте метод close класса FileMoveAction
          */
-       
+        throw new UnsupportedOperationException("Not implemented yet!");
     }
 
 }
